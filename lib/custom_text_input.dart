e091b954dc353238eda84dcd88103501b6bc9d5e
lucaschmider <47711576+lucaschmider@users.noreply.github.com>
2022-02-12 10:14:53 +0100

    added dirty check for validation erros

--- lib/custom_text_input.dart
+++ lib/custom_text_input.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomTextInput extends StatelessWidget {
+class CustomTextInput extends StatefulWidget {
   final TextEditingController controller;
   final String labelText;
   final bool isPassword;
@@ -19,18 +19,25 @@ class CustomTextInput extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CustomTextInput> createState() => _CustomTextInputState();
+}
+
+class _CustomTextInputState extends State<CustomTextInput> {
+  bool isDirty = false;
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(8.0),
         child: TextField(
-          obscureText: isPassword,
-          enableSuggestions: !isPassword,
-          autocorrect: !isPassword,
-          maxLines: maxLines,
-          controller: controller,
+          obscureText: widget.isPassword,
+          enableSuggestions: !widget.isPassword,
+          autocorrect: !widget.isPassword,
+          maxLines: widget.maxLines,
+          controller: widget.controller,
           decoration: InputDecoration(
-            labelText: labelText,
-            errorText: errorText,
+            labelText: widget.labelText,
+            errorText: isDirty ? widget.errorText : null,
             border: const OutlineInputBorder(
               borderSide: BorderSide.none,
               borderRadius: BorderRadius.all(Radius.circular(20.0)),
@@ -38,7 +45,14 @@ class CustomTextInput extends StatelessWidget {
             fillColor: Colors.grey.shade200,
             filled: true,
           ),
-          onChanged: onChanged,
+          onChanged: (e) {
+            setState(() {
+              isDirty = true;
+              if (widget.onChanged != null) {
+                widget.onChanged!(e);
+              }
+            });
+          },
         ));
   }
 }
