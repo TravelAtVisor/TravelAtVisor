5da3bd7377d817355747e1c58f0a9f8ae85dbfdc
Felix Sommerer <sommeref@dhbw-loerrach.de>
2022-02-21 16:24:07 +0100

    Models activity and trip

---
+++ lib/trip_management/activity.dart
@@ -0,0 +1,13 @@
+
+
+class Activity{
+  String activityId;
+  String foursqareId;
+  DateTime timestamp;
+  String title;
+  String description;
+  String photoUrl;
+
+  Activity(this.activityId, this.foursqareId, this.timestamp, this.title,
+      this.description, this.photoUrl);
+}
