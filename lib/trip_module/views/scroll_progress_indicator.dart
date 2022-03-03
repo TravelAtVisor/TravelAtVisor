ad5e3d795bc30eff7649f604f092577a41cd7342
Luca Schmider <schmidlu@dhbw-loerrach.de>
2022-03-03 19:55:55 +0100

    fixed slivering

--- lib/trip_module/views/scroll_progress_indicator.dart
+++ lib/trip_module/views/scroll_progress_indicator.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ScrollProgressIndicator extends StatelessWidget {
+  static const dotSize = 10.0;
   final int elementCount;
   final int currentElement;
 
@@ -15,25 +16,30 @@ class ScrollProgressIndicator extends StatelessWidget {
     final baseColor = Theme.of(context).brightness == Brightness.dark
         ? Colors.white
         : Colors.black;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(
-        elementCount,
-        (index) {
-          final distanceFactor =
-              pow(0.7, (index - currentElement).abs()).toDouble();
-          return AnimatedContainer(
-            duration: const Duration(milliseconds: 350),
-            curve: Curves.easeInOut,
-            width: 10 * distanceFactor,
-            height: 10 * distanceFactor,
-            margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: baseColor.withOpacity(currentElement == index ? 0.9 : 0.4),
-            ),
-          );
-        },
+    return SizedBox(
+      height: 26,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(
+          elementCount,
+          (index) {
+            final distanceFactor =
+                pow(0.7, (index - currentElement).abs()).toDouble();
+            return AnimatedContainer(
+              duration: const Duration(milliseconds: 350),
+              curve: Curves.easeInOut,
+              width: dotSize * distanceFactor,
+              height: dotSize * distanceFactor,
+              margin:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color:
+                    baseColor.withOpacity(currentElement == index ? 0.9 : 0.4),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
