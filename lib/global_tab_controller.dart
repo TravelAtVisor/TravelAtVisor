5d2935762a4d776a1b98a20aca9675e2be328050
Nick Schupp <schuppn@dhbw-loerrach.de>
2022-04-03 19:28:39 +0200

    Solution 2

--- lib/global_tab_controller.dart
+++ lib/global_tab_controller.dart
@@ -1,5 +1,6 @@
 import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:provider/provider.dart';
 import 'package:spincircle_bottom_bar/modals.dart';
 import 'package:spincircle_bottom_bar/spincircle_bottom_bar.dart';
@@ -44,6 +45,9 @@ class _GlobalTabControllerState extends State<GlobalTabController> {
   final PageStorageBucket bucket = PageStorageBucket();
   late String currentRoute;
 
+  var isDialOpen = ValueNotifier<bool>(false);
+  var extend = false;
+
   @override
   void initState() {
     currentRoute = widget.pageManager.initialRoute;
@@ -86,306 +90,80 @@ class _GlobalTabControllerState extends State<GlobalTabController> {
           ),
         ),*/
         //Solution 2:
-        body: PageStorage(
+        /*body: PageStorage(
           child: widget.pageManager.buildPage(currentRoute, context),
           bucket: bucket,
         ),
         floatingActionButton: ExpandableFab(
-          distance: 112.0,
+          distance: 80.0,
           children: [
             ActionButton(
-              onPressed: () => print('ABC'),
-              icon: const Icon(Icons.format_size),
-            ),
-            ActionButton(
-              onPressed: () => print('DEF'),
-              icon: const Icon(Icons.insert_photo),
-            ),
-            ActionButton(
-              onPressed: () => print('GHI'),
-              icon: const Icon(Icons.videocam),
-            ),
-            ActionButton(
-              onPressed: () => print('DEF'),
+              onPressed: () {},
               icon: const Icon(Icons.camera_enhance),
             ),
             ActionButton(
-              onPressed: () => print('GHI'),
+              onPressed: () {},
               icon: const Icon(Icons.camera),
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-@immutable
-class ExampleExpandableFab extends StatelessWidget {
-  static const _actionTitles = ['Create Post', 'Upload Photo', 'Upload Video'];
-
-  const ExampleExpandableFab({Key? key}) : super(key: key);
-
-  void _showAction(BuildContext context, int index) {
-    showDialog<void>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          content: Text(_actionTitles[index]),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('CLOSE'),
+        bottomNavigationBar: CustomTabBar(
+          routes: widget.pageManager.knownRoutes,
+          currentRoute: currentRoute,
+          onNavigated: (newRoute) => setState(
+                () {
+              currentRoute = newRoute;
+            },
+          ),
+        ),*/
+        //solution 3
+        body: PageStorage(
+          child: widget.pageManager.buildPage(currentRoute, context),
+          bucket: bucket,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: SpeedDial(
+          icon: Icons.add,
+          activeIcon: Icons.close,
+          spacing: 3,
+          openCloseDial: isDialOpen,
+          childPadding: const EdgeInsets.all(5),
+          spaceBetweenChildren: 4,
+          label: extend
+              ? const Text("Open")
+              : null,
+          elevation: 8.0,
+          isOpenOnStart: false,
+          animationSpeed: 200,
+          direction: SpeedDialDirection.up,
+          children: [
+            SpeedDialChild(
+              child: const Icon(Icons.accessibility),
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              label: 'First',
+              onTap: () => debugPrint('FIRST CHILD'),
+              onLongPress: () => debugPrint('FIRST CHILD LONG PRESS'),
             ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Expandable Fab'),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        itemCount: 25,
-        itemBuilder: (context, index) {
-          return Card();
-        },
-      ),
-      floatingActionButton: ExpandableFab(
-        distance: 112.0,
-        children: [
-          ActionButton(
-            onPressed: () => _showAction(context, 0),
-            icon: const Icon(Icons.format_size),
-          ),
-          ActionButton(
-            onPressed: () => _showAction(context, 1),
-            icon: const Icon(Icons.insert_photo),
-          ),
-          ActionButton(
-            onPressed: () => _showAction(context, 2),
-            icon: const Icon(Icons.videocam),
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
-  }
-}
-
-@immutable
-class ExpandableFab extends StatefulWidget {
-  const ExpandableFab({
-    Key? key,
-    this.initialOpen,
-    required this.distance,
-    required this.children,
-  }) : super(key: key);
-
-  final bool? initialOpen;
-  final double distance;
-  final List<Widget> children;
-
-  @override
-  _ExpandableFabState createState() => _ExpandableFabState();
-}
-
-class _ExpandableFabState extends State<ExpandableFab>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final Animation<double> _expandAnimation;
-  bool _open = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _open = widget.initialOpen ?? false;
-    _controller = AnimationController(
-      value: _open ? 1.0 : 0.0,
-      duration: const Duration(milliseconds: 250),
-      vsync: this,
-    );
-    _expandAnimation = CurvedAnimation(
-      curve: Curves.fastOutSlowIn,
-      reverseCurve: Curves.easeOutQuad,
-      parent: _controller,
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _toggle() {
-    setState(() {
-      _open = !_open;
-      if (_open) {
-        _controller.forward();
-      } else {
-        _controller.reverse();
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Stack(
-        alignment: Alignment.bottomRight,
-        clipBehavior: Clip.none,
-        children: [
-          _buildTapToCloseFab(),
-          ..._buildExpandingActionButtons(),
-          _buildTapToOpenFab(),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTapToCloseFab() {
-    return SizedBox(
-      width: 56.0,
-      height: 56.0,
-      child: Center(
-        child: Material(
-          shape: const CircleBorder(),
-          clipBehavior: Clip.antiAlias,
-          elevation: 4.0,
-          child: InkWell(
-            onTap: _toggle,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(
-                Icons.close,
-                color: Theme.of(context).primaryColor,
+            if (currentRoute == "trip_list") SpeedDialChild(
+                child: const Icon(Icons.brush),
+                backgroundColor: Colors.deepOrange,
+                foregroundColor: Colors.white,
+                label: 'Second',
+                onTap: () => debugPrint('SECOND CHILD'),
               ),
-            ),
+          ],
+        ),
+        bottomNavigationBar: CustomTabBar(
+          routes: widget.pageManager.knownRoutes,
+          currentRoute: currentRoute,
+          onNavigated: (newRoute) => setState(
+                () {
+                  debugPrint('Current:' + currentRoute);
+                  debugPrint('New:' + newRoute);
+              currentRoute = newRoute;
+            },
           ),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _buildExpandingActionButtons() {
-    final children = <Widget>[];
-    final count = widget.children.length;
-    final step = 90.0 / (count - 1);
-    for (var i = 0, angleInDegrees = 0.0;
-    i < count;
-    i++, angleInDegrees += step) {
-      children.add(
-        _ExpandingActionButton(
-          directionInDegrees: angleInDegrees,
-          maxDistance: widget.distance,
-          progress: _expandAnimation,
-          child: widget.children[i],
-        ),
-      );
-    }
-    return children;
-  }
-
-  Widget _buildTapToOpenFab() {
-    return IgnorePointer(
-      ignoring: _open,
-      child: AnimatedContainer(
-        transformAlignment: Alignment.center,
-        transform: Matrix4.diagonal3Values(
-          _open ? 0.7 : 1.0,
-          _open ? 0.7 : 1.0,
-          1.0,
-        ),
-        duration: const Duration(milliseconds: 250),
-        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
-        child: AnimatedOpacity(
-          opacity: _open ? 0.0 : 1.0,
-          curve: const Interval(0.25, 1.0, curve: Curves.easeInOut),
-          duration: const Duration(milliseconds: 250),
-          child: FloatingActionButton(
-            onPressed: _toggle,
-            child: const Icon(Icons.create),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-@immutable
-class _ExpandingActionButton extends StatelessWidget {
-  _ExpandingActionButton({
-    Key? key,
-    required this.directionInDegrees,
-    required this.maxDistance,
-    required this.progress,
-    required this.child,
-  }) : super(key: key);
-
-  final double directionInDegrees;
-  final double maxDistance;
-  final Animation<double> progress;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: progress,
-      builder: (context, child) {
-        final offset = Offset.fromDirection(
-          directionInDegrees * (math.pi / 180.0),
-          progress.value * maxDistance,
-        );
-        return Positioned(
-          right: 4.0 + offset.dx,
-          bottom: 4.0 + offset.dy,
-          child: Transform.rotate(
-            angle: (1.0 - progress.value) * math.pi / 2,
-            child: child!,
-          ),
-        );
-      },
-      child: FadeTransition(
-        opacity: progress,
-        child: child,
-      ),
-    );
-  }
-}
-
-@immutable
-class ActionButton extends StatelessWidget {
-  const ActionButton({
-    Key? key,
-    required this.icon,
-    this.tooltip,
-    this.onPressed,
-  }) : super(key: key);
-
-  final VoidCallback? onPressed;
-  final Widget icon;
-  final String? tooltip;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Material(
-      shape: const CircleBorder(),
-      clipBehavior: Clip.antiAlias,
-      color: theme.accentColor,
-      elevation: 4.0,
-      child: IconTheme.merge(
-        data: theme.accentIconTheme,
-        child: IconButton(
-          tooltip: tooltip,
-          onPressed: onPressed,
-          icon: icon,
         ),
       ),
     );
